e7afcbe2ea51084424443dfc088c8340f1fa9038
Balaji M A <Balaji.anandhababu@gmail.com>
2023-11-12 03:19:12 +0530

    wallet balance

--- js/common.js
+++ js/common.js
@@ -160,6 +160,11 @@ function load_admin_sidenavbar(active_nav) {
             <i class="fa fa-search fa-lg"></i> Payin Cashback\
           </a>\
         </li>\
+		<li id="nav-wallet-balance">\
+          <a href="./wallet-balance.html">\
+            <i class="fa fa-tachometer-alt fa-lg"></i> Profit Share\
+          </a>\
+        </li>\
 		<li id="nav-business-summary">\
           <a href="./business-summary.html">\
             <i class="fa fa-tachometer-alt fa-lg"></i> Profit Share\
